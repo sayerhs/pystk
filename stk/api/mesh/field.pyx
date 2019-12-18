05e316b75c2dfa6b06b1b52c896686d3056049bd
Shreyas Ananthan <shreyas@umd.edu>
2019-12-18 11:01:39 -0700

    Add field data accessors and part registration logic

--- stk/api/mesh/field.pyx
+++ stk/api/mesh/field.pyx
@@ -14,23 +14,13 @@ from .entity cimport StkEntity
 cimport numpy as np
 import numpy as np
 
-cdef dict field_state_map = dict(
-    StateNew = StateNew,
-    StateNP1 = StateNP1,
-    StateNone = StateNone,
-    StateN = StateN,
-    StateOld = StateOld,
-    StateNM1 = StateNM1,
-    StateNM2 = StateNM2,
-    StateNM3 = StateNM3,
-    StateNM4 = StateNM4
-)
-
-
 cdef class StkFieldBase:
 
     def __cinit__(self):
         self.fld = NULL
+
+    def __eq__(self, StkFieldBase other):
+        return self.fld == other.fld
 
     @staticmethod
     cdef wrap_instance(FieldBase* fld):
@@ -83,5 +73,55 @@ cdef class StkFieldBase:
         assert (fld != NULL), "Invalid field state encountered"
         return StkFieldBase.wrap_instance(fld)
 
+    def get(self, StkEntity entity):
+        """Get the data for a given entity"""
+        cdef Entity sentity = entity.entity
+        cdef void* ptr = field_data(deref(self.fld), sentity)
+        cdef unsigned ncomp = field_scalars_per_entity(deref(self.fld), sentity)
+        return np.asarray(<double[:ncomp]>ptr)
+
+    def bkt_view(self, StkBucket bkt):
+        """Get the data view for a bucket"""
+        cdef Bucket* sbkt = bkt.bkt
+        cdef void* ptr = field_data(deref(self.fld), deref(sbkt))
+        cdef unsigned ncomp = field_scalars_per_entity(deref(self.fld), deref(sbkt))
+        if ncomp == 1:
+            return np.asarray(<double[:bkt.size]>ptr)
+        else:
+            return np.asarray(<double[:bkt.size, :ncomp]>ptr)
+
+    def get_t(self, StkEntity entity, cython.numeric dtype=0):
+        """Get the data for a given entity"""
+        cdef Entity sentity = entity.entity
+        cdef void* ptr = field_data(deref(self.fld), sentity)
+        cdef unsigned ncomp = field_scalars_per_entity(deref(self.fld), sentity)
+        return np.asarray(<cython.numeric[:ncomp]>ptr)
+
+    def bkt_view_t(self, StkBucket bkt, cython.numeric dtype=0):
+        """Get the data view for a bucket"""
+        cdef Bucket* sbkt = bkt.bkt
+        cdef void* ptr = field_data(deref(self.fld), deref(sbkt))
+        cdef unsigned ncomp = field_scalars_per_entity(deref(self.fld), deref(sbkt))
+        if ncomp == 1:
+            return np.asarray(<cython.numeric[:bkt.size]>ptr)
+        else:
+            return np.asarray(<cython.numeric[:bkt.size, :ncomp]>ptr)
+
+    def add_to_part(self, StkPart part, int num_components=1, double[:] init_value=None):
+        """Register field to a given part"""
+        cdef Part* spart = part.part
+        cdef FieldBase* sfield = self.fld
+        cdef double* init_ptr = NULL
+        if init_value is not None:
+            assert num_components == init_value.shape[0], "Size mismatch in initial value"
+            init_ptr = &init_value[0]
+        if num_components == 1:
+            put_field_on_mesh(deref(sfield), deref(spart), init_ptr)
+        else:
+            put_field_on_mesh(deref(sfield), deref(spart), num_components, init_ptr)
+
     def __repr__(self):
-        return "<%s: %s>"%(self.__class__.__name__, self.name)
+        if self.fld != NULL:
+            return "<%s: %s>"%(self.__class__.__name__, self.name)
+        else:
+            return "<%s: %s>"%(self.__class__.name__, "INVALID_FIELD")
